29b1b24dacb0911515d2b892164863ada7b54a68
cjcchen <ickchenjunchao@gmail.com>
2022-09-23 22:29:39 +0800

    add condition for storage to reduce the build time.

--- kv_server/kv_server_executor.cpp
+++ kv_server/kv_server_executor.cpp
@@ -8,11 +8,16 @@ namespace resdb {
 
 KVServerExecutor::KVServerExecutor(const ResConfigData& config_data,
                                    char* cert_file)
+#ifdef EnableStorage
     : l_storage_layer_(cert_file, config_data),
       r_storage_layer_(cert_file, config_data) {
   equip_rocksdb_ = config_data.rocksdb_info().enable_rocksdb();
   equip_leveldb_ = config_data.leveldb_info().enable_leveldb();
 }
+#else 
+{
+}
+#endif
 
 KVServerExecutor::KVServerExecutor(void) {}
 
@@ -41,6 +46,7 @@ std::unique_ptr<std::string> KVServerExecutor::ExecuteData(
 }
 
 void KVServerExecutor::Set(const std::string& key, const std::string& value) {
+#ifdef EnableStorage
   if (equip_rocksdb_) {
     r_storage_layer_.setDurable(key, value);
   } else if (equip_leveldb_) {
@@ -48,14 +54,21 @@ void KVServerExecutor::Set(const std::string& key, const std::string& value) {
   } else {
     kv_map_[key] = value;
   }
+#else
+    kv_map_[key] = value;
+#endif
 }
 
 std::string KVServerExecutor::Get(const std::string& key) {
+#ifdef EnableStorage
   if (equip_rocksdb_)
     return r_storage_layer_.getDurable(key);
   else if (equip_leveldb_)
     return l_storage_layer_.getDurable(key);
   else
     return kv_map_[key];
+#else
+    return kv_map_[key];
+#endif
 }
 }  // namespace resdb
