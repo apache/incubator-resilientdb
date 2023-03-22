73a48409c8a158419506efb31775971ea57a6bbf
Apratim Shukla <apratimshukla6@gmail.com>
2023-03-22 08:53:03 -0700

    Popup added

---
+++ public/background.js
@@ -0,0 +1,44 @@
+/*global chrome*/
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostContains: '' }
+        })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("onMessage");
+    if (request.action === 'showExtension') {
+        chrome.storage.local.get("popupShown", function(data) {
+        if (!data.popupShown) {
+            chrome.windows.getCurrent(function(currentWindow) {
+                var left = currentWindow.left;
+                var top = currentWindow.top;
+                var width = currentWindow.width;
+            
+                // Show the page action popup at the same position as the extension button
+                chrome.windows.create({
+                  url: chrome.extension.getURL("index.html"),
+                  type: "popup",
+                  top: 65,
+                  left: 840,
+                  height: 558,
+                  width: 400
+                });
+            });
+            // Save popupShown flag in storage
+            chrome.storage.local.set({ "popupShown": true });
+        }
+    });
+    }
+});
+
+// Reset popupShown flag when popup is closed
+chrome.windows.onRemoved.addListener(function() {
+    chrome.storage.local.set({ "popupShown": false });
+});
--- public/content-script.js
+++ public/content-script.js
@@ -1,22 +1,27 @@
 /*global chrome*/
 window.addEventListener("message", (event) => {
   if (
-    event.source === window &&
-    event.data.direction === "from-page-script"
+      event.source === window &&
+      event.data.direction === "from-page-script"
   ) {
-    chrome.runtime.sendMessage({
+      chrome.runtime.sendMessage({
       from: 'content',
       data: event.data.message,
       amount: event.data.amount,
       address: event.data.address,
       event: event.data.event
-    })
+      })
   }
   else if (event.source === window &&
-    event.data.direction === "id-page-script") {
+      event.data.direction === "id-page-script") {
       chrome.runtime.sendMessage({
-        from: 'fetch',
-        id: event.data.id
+          from: 'fetch',
+          id: event.data.id
       })
-    }
+  }
+});
+
+document.getElementById('from-page-script').addEventListener('click', function() {
+  console.log("sendMessage");
+  chrome.runtime.sendMessage({ action: 'showExtension' });
 });
