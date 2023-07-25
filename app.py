ebc4586b7c71ee30864e24eacb7b4a1ef3ec0005
Apratim Shukla <apratimshukla6@gmail.com>
2023-07-24 20:51:42 -0700

    Update Mutation and Fetch Query

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from resdb_driver import Resdb
 from resdb_driver.crypto import generate_keypair
 
 db_root_url = "localhost:18000"
+protocol = "http://"
+fetch_all_endpoint = "/v1/transactions"
 db = Resdb(db_root_url)
 
 import strawberry
 import typing
 import ast
+from typing import Optional, List
+from filter import filter_by_keys
 
 from flask import Flask
 from flask_cors import CORS
@@ -41,6 +45,11 @@ class PrepareAsset:
     recipientPublicKey: str
     asset: str
 
+@strawberry.input
+class FilterKeys:
+    ownerPublicKey: Optional[str]
+    recipientPublicKey: Optional[str]
+
 @strawberry.type
 class Keys:
     publicKey: str
@@ -63,12 +72,31 @@ class Query:
             asset=str(data["asset"])
         )
         return payload
+    
+    @strawberry.field
+    def getFilteredTransactions(self, filter: Optional[FilterKeys]) -> List[RetrieveTransaction]:
+        print(filter.recipientPublicKey)
+        url = f"{protocol}{db_root_url}{fetch_all_endpoint}"
+        json_data = filter_by_keys(url, filter.ownerPublicKey, filter.recipientPublicKey)
+        records = []
+        for data in json_data:
+            records.append(RetrieveTransaction(
+            id=data["id"],
+            version=data["version"],
+            amount=data["outputs"][0]["amount"],
+            uri=data["outputs"][0]["condition"]["uri"],
+            type=data["outputs"][0]["condition"]["details"]["type"],
+            publicKey=data["outputs"][0]["condition"]["details"]["public_key"],
+            operation=data["operation"],
+            metadata=data["metadata"],
+            asset=str(data["asset"])
+            ))
+        return records
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
     def postTransaction(self, data: PrepareAsset) -> CommitTransaction:
-        print(data.operation)
         prepared_token_tx = db.transactions.prepare(
         operation=data.operation,
         signers=data.signerPublicKey,
@@ -82,6 +110,33 @@ class Mutation:
         id = db.transactions.send_commit(fulfilled_token_tx)[4:] # Extract ID
         payload = CommitTransaction(
             id=id
+        )
+        return payload
+    
+    @strawberry.mutation
+    def updateTransaction(self, data: PrepareAsset) -> RetrieveTransaction:
+        prepared_token_tx = db.transactions.prepare(
+        operation=data.operation,
+        signers=data.signerPublicKey,
+        recipients=[([data.recipientPublicKey], data.amount)],
+        asset=ast.literal_eval(data.asset),
+        )
+
+        # fulfill the tnx
+        fulfilled_token_tx = db.transactions.fulfill(prepared_token_tx, private_keys=data.signerPrivateKey)
+
+        id = db.transactions.send_commit(fulfilled_token_tx)[4:] # Extract ID
+        data = db.transactions.retrieve(txid=id)
+        payload = RetrieveTransaction(
+            id=data["id"],
+            version=data["version"],
+            amount=data["outputs"][0]["amount"],
+            uri=data["outputs"][0]["condition"]["uri"],
+            type=data["outputs"][0]["condition"]["details"]["type"],
+            publicKey=data["outputs"][0]["condition"]["details"]["public_key"],
+            operation=data["operation"],
+            metadata=data["metadata"],
+            asset=str(data["asset"])
         )
         return payload
 
---
+++ filter.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+def fix_json_with_commas(json_str):
+    fixed_json = json_str.replace('}{', '},{')
+    return f"[{fixed_json}]"
+
+def get_json_objects_by_public_key(json_data, owner_public_key=None, recipient_public_key=None):
+    matching_objects = []
+    if owner_public_key != None and recipient_public_key != None:
+        for obj in json_data[0]:
+            if owner_public_key in obj['inputs'][0]['owners_before'] and recipient_public_key in obj['outputs'][0]['public_keys']:
+                matching_objects.append(obj)
+    elif owner_public_key == None and recipient_public_key != None:
+        for obj in json_data[0]:
+            if recipient_public_key in obj['outputs'][0]['public_keys']:
+                matching_objects.append(obj)
+    elif owner_public_key != None and recipient_public_key == None:
+        for obj in json_data[0]:
+            if owner_public_key in obj['inputs'][0]['owners_before']:
+                matching_objects.append(obj)
+    return matching_objects
+
+def get_json_data(url, ownerPublicKey=None, recipientPublicKey=None):
+    try:
+        response = requests.get(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON data from the response
+            json_text = fix_json_with_commas(response.text)
+            json_data = json.loads(json_text)
+            if ownerPublicKey == None and recipientPublicKey == None:
+                return json_data
+            elif ownerPublicKey == None and recipientPublicKey != None:
+                matching_objects = get_json_objects_by_public_key(json_data, None, recipientPublicKey)
+                return matching_objects
+            elif ownerPublicKey != None and recipientPublicKey == None:
+                matching_objects = get_json_objects_by_public_key(json_data, ownerPublicKey, None)
+                return matching_objects
+            else:
+                # Get all JSON objects that match the given publicKey
+                matching_objects = get_json_objects_by_public_key(json_data, ownerPublicKey, recipientPublicKey)
+                return matching_objects
+        else:
+            print(f"Error: Unable to retrieve data from {url}. Status code: {response.status_code}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+def filter_by_keys(url, ownerPublicKey, recipientPublicKey):
+    json_data = get_json_data(url, ownerPublicKey, recipientPublicKey)
+    return json_data
