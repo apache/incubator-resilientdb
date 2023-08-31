56a963e0c932d449aec4106f80c48ca4f7abf7c2
cjcchen <ickchenjunchao@gmail.com>
2023-08-31 07:15:25 +0000

    add

--- src/index.js
+++ src/index.js
@@ -119,9 +119,6 @@ getFiles(dir, patterns, ignore).then( async ret => {
 	core.setOutput("total_lines", `${ret.lines}`);
 	core.setOutput("ignored_files", `${ret.ignored}`);
 	core.setOutput("counted_files", `${ret.counted}`);
-	core.setOutput("elapsed_ms", `${Date.now() - st}`);
-	core.setOutput("output_path", `${path.resolve(badge)}`);
-	core.setOutput("output_dir", `${path.resolve(path.dirname(badge))}`);
 
 	await fs.mkdir(path.dirname(badge), { recursive: true })
 
