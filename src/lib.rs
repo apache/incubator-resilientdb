3fc48c18aeb239177140a5899ecbc72b3bd3093f
Dhruv Sangamwar <dsangamwar@gmail.com>
2023-11-11 13:23:21 -0800

    Async API code to call HTTP Endpoint - NOT TESTED

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,61 @@
 // This is the main entry point for the library.
 
-mod my_module;
+use reqwest::Error;
+use serde::Deserialize;
 
-pub fn add_numbers(a: i32, b: i32) -> i32 {
-    my_module::add_internal(a, b)
+#[derive(Debug, Deserialize)]
+struct ApiResponse {
+    inputs: Vec<Input>,
+    outputs: Vec<Output>,
+    operation: String,
+    metadata: Option<serde_json::Value>,
+    asset: Asset,
+    version: String,
+    id: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Input {
+    owners_before: Vec<String>,
+    fulfills: Option<serde_json::Value>,
+    fulfillment: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Output {
+    public_keys: Vec<String>,
+    condition: Condition,
+    amount: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Condition {
+    details: ConditionDetails,
+    uri: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ConditionDetails {
+    #[serde(rename = "type")]
+    condition_type: String,
+    public_key: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Asset {
+    data: serde_json::Value,
+}
+
+pub async fn get_data_from_api(api_url: &str) -> Result<ApiResponse, Error> {
+    // Make an asynchronous GET request to the specified API endpoint
+    let response = reqwest::get(api_url).await?;
+
+    // Check if the response has a successful status code
+    response.error_for_status()?;
+
+    // If successful, deserialize the JSON response
+    let response_text = response.text().await?;
+    let response: ApiResponse = serde_json::from_str(&response_text)?;
+
+    Ok(response)
 }
--- testsdk/src/main.rs
+++ testsdk/src/main.rs
@@ -1,8 +1,19 @@
 // This is the main file for a project that uses the library.
-
 extern crate resdb_rust_sdk;
+use tokio::main;
 
 fn main() {
-    let result = resdb_rust_sdk::add_numbers(5, 7);
-    println!("Result: {}", result);
+    // Specify the URL of the JSON API endpoint
+    let api_url = "https://crow.resilientdb.com/v1/transactions";
+
+    match my_rest_library::get_data_from_api(api_url).await {
+        Ok(response) => {
+            println!("Response: {:?}", response);
+            // Handle the response data as needed
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            // Handle the error
+        }
+    }
 }
