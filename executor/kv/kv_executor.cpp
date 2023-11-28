0f1ec7bea8d2ed8886700c8073c5c17ed375b172
Ubuntu <ubuntu@ip-172-31-16-70.ec2.internal>
2023-11-28 23:40:27 +0000

    add mvcc

--- executor/kv/kv_executor.cpp
+++ executor/kv/kv_executor.cpp
@@ -27,12 +27,11 @@
 
 #include <glog/logging.h>
 
-#include "proto/kv/kv.pb.h"
 
 namespace resdb {
 
-KVExecutor::KVExecutor(std::unique_ptr<ChainState> state)
-    : state_(std::move(state)) {}
+KVExecutor::KVExecutor(std::unique_ptr<Storage> storage)
+    : storage_(std::move(storage)) {}
 
 std::unique_ptr<std::string> KVExecutor::ExecuteData(
     const std::string& request) {
@@ -52,7 +51,18 @@ std::unique_ptr<std::string> KVExecutor::ExecuteData(
     kv_response.set_value(GetAllValues());
   } else if (kv_request.cmd() == KVRequest::GETRANGE) {
     kv_response.set_value(GetRange(kv_request.key(), kv_request.value()));
+  } else if (kv_request.cmd() == KVRequest::SET_WITH_VERSION) {
+    SetWithVersion(kv_request.key(), kv_request.value(), kv_request.version());
+  } else if (kv_request.cmd() == KVRequest::GET_WITH_VERSION) {
+    GetWithVersion(kv_request.key(), kv_request.version(), kv_response.mutable_value_info());
+  } else if (kv_request.cmd() == KVRequest::GET_ALL_ITEMS) {
+    GetAllItems(kv_response.mutable_items());
+  } else if (kv_request.cmd() == KVRequest::GET_KEY_RANGE) {
+    GetKeyRange(kv_request.min_key(), kv_request.max_key(), kv_response.mutable_items());
+  } else if (kv_request.cmd() == KVRequest::GET_HISTORY) {
+    GetHistory(kv_request.key(), kv_request.min_version(), kv_request.max_version(), kv_response.mutable_items());
   }
+
 
   std::unique_ptr<std::string> resp_str = std::make_unique<std::string>();
   if (!kv_response.SerializeToString(resp_str.get())) {
@@ -63,19 +73,63 @@ std::unique_ptr<std::string> KVExecutor::ExecuteData(
 }
 
 void KVExecutor::Set(const std::string& key, const std::string& value) {
-  state_->SetValue(key, value);
+  storage_->SetValue(key, value);
 }
 
 std::string KVExecutor::Get(const std::string& key) {
-  return state_->GetValue(key);
+  return storage_->GetValue(key);
 }
 
-std::string KVExecutor::GetAllValues() { return state_->GetAllValues(); }
+std::string KVExecutor::GetAllValues() { return storage_->GetAllValues(); }
 
 // Get values on a range of keys
 std::string KVExecutor::GetRange(const std::string& min_key,
                                  const std::string& max_key) {
-  return state_->GetRange(min_key, max_key);
+  return storage_->GetRange(min_key, max_key);
 }
+
+void KVExecutor::SetWithVersion(const std::string& key, const std::string& value, int version) {
+  storage_->SetValueWithVersion(key, value, version);
+}
+
+void KVExecutor::GetWithVersion(const std::string& key, int version, ValueInfo* info) {
+  std::pair<std::string, int> ret = storage_->GetValueWithVersion(key, version);
+  info->set_value(ret.first);
+  info->set_version(ret.second);
+}
+
+void KVExecutor::GetAllItems(Items * items) {
+  const std::map<std::string,std::pair<std::string,int>>& ret = storage_->GetAllItems();
+  for( auto it : ret) {
+    Item * item = items->add_item();
+    item->set_key(it.first);
+    item->mutable_value_info()->set_value(it.second.first);
+    item->mutable_value_info()->set_version(it.second.second);
+  }
+}
+
+void KVExecutor::GetKeyRange(const std::string& min_key, 
+    const std::string& max_key, Items * items) {
+  const std::map<std::string,std::pair<std::string,int>>& ret = storage_->GetKeyRange(min_key, max_key);
+  for( auto it : ret) {
+    Item * item = items->add_item();
+    item->set_key(it.first);
+    item->mutable_value_info()->set_value(it.second.first);
+    item->mutable_value_info()->set_version(it.second.second);
+  }
+}
+
+void KVExecutor::GetHistory(const std::string& key, int min_version, 
+    int max_version, Items * items) {
+  const std::vector<std::pair<std::string,int>>& ret = storage_->GetHistory(
+    key, min_version, max_version);
+  for( auto it : ret) {
+    Item * item = items->add_item();
+    item->set_key(key);
+    item->mutable_value_info()->set_value(it.first);
+    item->mutable_value_info()->set_version(it.second);
+  }
+}
+
 
 }  // namespace resdb
