3bed8178ad423caccf84161b48a77ab52eaabfca
Gopal Nambiar <gopalnambiar2@gmail.com>
2023-12-05 07:35:28 -0800

    Added latest changes

--- resdb_cli.py
+++ resdb_cli.py
@@ -4,27 +4,28 @@ import os
 import subprocess
 import configparser
 import bcrypt
-import json
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-CONFIG_FILE_PATH = os.path.expanduser("~/.resdb_config")
+import requests
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-MONGODB_URI = config.get('Database', 'MongoDB_URI')
-DATABASE_NAME = config.get('Database', 'Name')
+FLASK_BASE_URL = config.get('Server', 'flask_base_url')
 CURRENT_USER = config.get('User', 'Current_User')
 
 
-def get_database():
-    client = MongoClient(MONGODB_URI, server_api=ServerApi("1"))
-    return client[DATABASE_NAME]
+def make_flask_request(endpoint, method, data=None):
+    url = f"{FLASK_BASE_URL}/{endpoint}"
+    headers = {"Content-Type": "application/json"}
+    response = requests.request(method, url, json=data, headers=headers)
+
+    if not response.ok:
+        response.raise_for_status()
+
+    return response.json()
 
 
 def get_logged_in_user():
-    return CURRENT_USER
+    return config.get('User', 'Current_User')
 
 
 def set_logged_in_user(username):
@@ -47,30 +48,31 @@ def cli():
 @click.option("--password", prompt=True, hide_input=True)
 def login(username, password):
     """Login to your account"""
-    # Implement login logic
-    db = get_database()
-    users_collection = db["users"]
+    try:
+        response = make_flask_request(
+            "getUser", "POST", {"email": username, "password": password})
 
-    # Example: Check if the user exists
-    user = users_collection.find_one({"email": username})
-
-    if user and verify_password(password, user["password"]):
-        # Login successful
-        # You may want to set a session or token for the user here
-        set_logged_in_user(username)
-        click.echo("Login successful")
-    else:
-        # Login failed
-        click.echo("Login failed")
+        if response.get("success"):
+            # Login successful
+            set_logged_in_user(username)
+            click.echo("Login successful")
+        else:
+            # Login failed
+            click.echo("Login failed")
+    except Exception:
+        click.echo(f"Incorrect username or password")
 
 
 @cli.command()
 def logout():
     """Logout from the current account"""
-    config.set('User', 'Current_User', '')
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-    click.echo('Logout successful')
+    try:
+        config.set('User', 'Current_User', '')
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        click.echo('Logout successful')
+    except Exception:
+        click.echo('Logout failed')
 
 
 @cli.command()
@@ -104,33 +106,21 @@ def create_instance(type):
 
         subprocess.run(command, check=True)
 
-        # Store instance details in MongoDB
-        db = get_database()
-        instances_collection = db["instances"]
+        response = make_flask_request(
+            "updateInstances", "POST", {"user_email": current_user, "instance": type, "inc": True})       
 
-        # Check if there is an existing entry for the current user
-        existing_entry = instances_collection.find_one({"user": current_user})
-
-        if existing_entry:
-            # Update the existing entry
-            instances_collection.update_one(
-                {"user": current_user},
-                {"$set": {type: existing_entry.get(type, 0) + 1}},
-            )
+        if response.get("success"):
             click.echo(
                 f"Instance created successfully. Container Name: {container_name}"
             )
         else:
-            # Create a new entry for the current user
-            new_entry = {"user": current_user, type: 1}
-            instances_collection.insert_one(new_entry)
-            click.echo(
-                f"Instance created successfully. New entry created. Container Name: {container_name}"
-            )
+            click.echo("Creation failed")
+
     except subprocess.CalledProcessError as error:
         click.echo(f"Error creating instance: {error}", err=True)
     except Exception as error:
         click.echo(f"An unexpected error occurred: {error}", err=True)
+
 
 @cli.command()
 @click.argument("instance_id")
@@ -151,7 +141,7 @@ def exec_into(instance_id):
         click.echo(f"Error executing command: {error}", err=True)
     except Exception as error:
         click.echo(f"An unexpected error occurred: {error}", err=True)
-    
+
 
 @cli.command()
 def view_instances():
@@ -204,15 +194,15 @@ def delete_instance(instance_id):
         # Remove the Docker container
         subprocess.run(["docker", "rm", instance_id], check=True)
 
-        # Remove instance details from MongoDB
-        db = get_database()
-        instances_collection = db["instances"]
+        response = make_flask_request(
+            "updateInstances", "POST", {"user_email": current_user, "instance": container_type, "inc": False})       
 
-        instances_collection.update_one(
-            {"user": current_user},
-            # Decrease the count by 1 (or update based on your requirements)
-            {"$inc": {container_type: -1}}
-        )
+        if response.get("success"):
+            click.echo(
+                f"Instance deleted successfully. Container Name: {container_name}"
+            )
+        else:
+            click.echo("Deletion failed")
 
         click.echo("Instance deleted successfully.")
     except subprocess.CalledProcessError as error:
