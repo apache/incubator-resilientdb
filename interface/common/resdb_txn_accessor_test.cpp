c3f73500b381415fb9eaff961e8e6c9ea2c209ff
Ubuntu <ubuntu@ip-172-31-45-3.ec2.internal>
2024-01-04 02:02:51 +0000

    ft

--- interface/common/resdb_txn_accessor_test.cpp
+++ interface/common/resdb_txn_accessor_test.cpp
@@ -60,9 +60,9 @@ TEST(ResDBTxnAccessorTest, GetTransactionsFail) {
       .WillRepeatedly(Invoke([&](const std::string& ip, int port) {
         auto client = std::make_unique<MockNetChannel>(ip, port);
 	LOG(ERROR)<<"get ip:"<<ip<<" port:"<<port;
-//EXPECT_CALL(*client,
- //                   SendRequest(EqualsProto(request), Request::TYPE_QUERY, _))
-  //          .WillOnce(Return(0));
+	EXPECT_CALL(*client,
+                    SendRequest(EqualsProto(request), Request::TYPE_QUERY, _))
+            .WillOnce(Return(0));
         EXPECT_CALL(*client,
                     SendRequest(_, _, _))
             .WillRepeatedly(Return(0));
