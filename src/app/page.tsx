e4794e43f534f920e9cfef577a89847ac2007f47
Bismanpal Singh <bisman.singh24@gmail.com>
2024-11-11 23:30:18 -0800

    added files

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+ 
+import Dashboard from "@/app/dashboard/page";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title> MemView </title> 
+      <meta name = "Memview" content = "dashboard"/>
+      <meta name = "viewport" content = "width=device-width, initial-scale=1"/>
+    </Head>
+    <main>
+      <Dashboard />
+    </main>
+    </>
+  );
+}
