8214f4281b927d1574129b6f72b89f9165ee29fd
Bismanpal Singh <bisman.singh24@gmail.com>
2024-11-15 20:37:57 -0800

    landing page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import { Main, Section, Container, Box } from "@/components/craft";
+import Hero from "@/components/home-page/hero";
 
 export default function Home() {
   return (
     <Main>
     <Section>
       <Container>
-        <h1>Heading</h1>
-        <p>Content</p>
-        <Box direction="row" wrap={true} gap={4}>
-          <div>Item 1</div>
-          <div>Item 2</div>
-        </Box>
+        <Hero/>
       </Container>
     </Section>
   </Main>
