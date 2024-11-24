2e0c80730591842c743362f7f5a3f654054f9548
Bismanpal Singh <bisman.singh24@gmail.com>
2024-11-23 21:37:42 -0800

    Adding authors page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from "@/components/dashboard/dashboard";
 import { HomePage } from "@/components/home-page/homePage";
 import PBFTPage from "@/components/home-page/PBFT"; // Import the new PBFT page
+import { ThreeDCardDemo } from "./components/Authors/Authorcard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/pbft" element={<PBFTPage />} /> {/* Add this */}
+          <Route path="/authors" element={<ThreeDCardDemo />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
