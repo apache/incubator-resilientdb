1a30c2741481d97c26fdc96b2f34b45b1a0e8b27
Abdel Ibrahim <abdelibrahim945@gmail.com>
2025-07-25 15:18:42 -0700

    #9 lint esline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/coreweb-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
