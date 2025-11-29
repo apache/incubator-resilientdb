2f092b4311912201aa259f700cf7a7494d99b1e0
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2025-11-29 09:42:06 +0000

    automated scripts

--- scripts/deploy/multiply_by_1000.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-input_file=$1
-awk 'NR==1 {print; next} {printf "%s", $1; for(i=2;i<=NF;i++) printf " %.10f", $i*1000; print ""}' "$input_file" > "${input_file}.tmp" && mv "${input_file}.tmp" "$input_file"ç
