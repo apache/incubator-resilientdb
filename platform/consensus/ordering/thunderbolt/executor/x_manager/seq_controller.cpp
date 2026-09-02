071190bc5f81bbd6d368751234b0d7957f6f0845
cjcchen <ickchenjunchao@gmail.com>
2026-09-02 17:26:31 +0800

    add header

--- platform/consensus/ordering/thunderbolt/executor/x_manager/seq_controller.cpp
+++ platform/consensus/ordering/thunderbolt/executor/x_manager/seq_controller.cpp
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 #include <glog/logging.h>
 
 #include <queue>
@@ -5,7 +23,7 @@
 
 #include "common/utils/utils.h"
 #include "eEVM/exception.h"
-#include "service/contract/executor/x_manager/x_controller.h"
+#include "platform/consensus/ordering/thunderbolt/executor/x_manager/x_controller.h"
 
 //#define CDebug
 //#define DDebug
--- platform/consensus/ordering/thunderbolt/executor/x_manager/streaming_e_controller.h
+++ platform/consensus/ordering/thunderbolt/executor/x_manager/streaming_e_controller.h
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 #pragma once
 
 #include <deque>
@@ -6,8 +24,8 @@
 #include <thread>
 
 #include "platform/common/queue/lock_free_queue.h"
-#include "service/contract/executor/x_manager/concurrency_controller.h"
-#include "service/contract/executor/x_manager/d_storage.h"
+#include "platform/consensus/ordering/thunderbolt/executor/x_manager/concurrency_controller.h"
+#include "platform/consensus/ordering/thunderbolt/executor/x_manager/d_storage.h"
 
 namespace resdb {
 namespace contract {
